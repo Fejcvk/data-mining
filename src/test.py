d47125546a9936df0e973d3adaf94544d4938fb0
Fejcvk <>
2020-04-01 16:42:41 +0200

    bert added

--- src/test.py
+++ src/test.py
@@ -1,1 +1,10 @@
-print('Hello from Docker')
+import torch
+from transformers import BertForMaskedLM, BertTokenizer, pipeline
+
+model = BertForMaskedLM.from_pretrained("dkleczek/bert-base-polish-uncased-v1")
+tokenizer = BertTokenizer.from_pretrained("dkleczek/bert-base-polish-uncased-v1")
+
+nlp = pipeline('fill-mask', model=model, tokenizer=tokenizer)
+
+for pred in nlp(f"Adam Mickiewicz wielkim polskim {nlp.tokenizer.mask_token} był."):
+  print(pred)
